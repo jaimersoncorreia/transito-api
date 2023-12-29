cdbb0bdef472b4eee33dba400fd264dd7ace625b
Jaimerson <jaimersonsantos@ufam.edu.br>
2023-12-29 17:27:41 -0400

    5.3. Customizando as informações do ProblemDetail

--- src/main/java/tech/bacuri/transito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/tech/bacuri/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,16 +1,35 @@
 package tech.bacuri.transito.api.exceptionhandler;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tech.bacuri.transito.domain.exception.NegocioException;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("http://localhost:8080/erros"));
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<Map<String, String>> capturar(NegocioException e) {
         Map<String, String> body = new HashMap<>();
