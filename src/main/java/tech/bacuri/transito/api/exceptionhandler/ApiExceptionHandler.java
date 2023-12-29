11a26bff490b3445fd94c4f8e78aa38811563e31
Jaimerson <jaimersonsantos@ufam.edu.br>
2023-12-29 17:13:30 -0400

    5.2. Usando a RFC 7807 (Problem Details for HTTP APIs)

--- src/main/java/tech/bacuri/transito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/tech/bacuri/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,13 +3,14 @@ package tech.bacuri.transito.api.exceptionhandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tech.bacuri.transito.domain.exception.NegocioException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<Map<String, String>> capturar(NegocioException e) {
         Map<String, String> body = new HashMap<>();
